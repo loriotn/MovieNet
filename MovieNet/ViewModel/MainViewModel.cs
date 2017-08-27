4c8026edceb74dd4ee49045da57dd6cfbefe4f18
unknown <loriot_n@etna-alternance.net>
2017-08-27 22:50:31 +0100

    add full navigation behavior

--- MovieNet/ViewModel/MainViewModel.cs
+++ MovieNet/ViewModel/MainViewModel.cs
@@ -12,19 +12,25 @@ namespace MovieNet.ViewModel
 {
     public class MainViewModel : ViewModelBase
     {
-        public RelayCommand NavigateTo { get; }
+        public RelayCommand<string> NavigateTo { get; private set; }
         private Service1 service;
         private List<Utilisateur> _utilisateurs;
         private Utilisateur _utilisateur;
+        private UserControl selectedView;
 
         public MainViewModel(Service1 service)
         {
             this.service = service;
-            NavigateTo = new RelayCommand(NavigateExecute, NavigateCanExecute);
+            NavigateTo = new RelayCommand<string>((param) => NavigateExecute(param), NavigateCanExecute);
         }
 
 
         private UserControl _currentView;
+
+        public void test()
+        {
+            selectedView = new Users();
+        }
 
         public UserControl CurrentView
         {
@@ -52,12 +58,24 @@ namespace MovieNet.ViewModel
             }
         }
         
-        public void NavigateExecute()
+        public void NavigateExecute(string parameter)
         {
-            CurrentView = new Users();
+            if (parameter.Equals("user"))
+            {
+                selectedView = new Users();
+            }
+            else if (parameter.Equals("film"))
+            {
+                selectedView = new Films();
+            }
+            else
+            {
+                selectedView = null;
+            }
+            CurrentView = selectedView;
         }
 
-        public bool NavigateCanExecute() { return true; }
+        public bool NavigateCanExecute(string p) { return true; }
         public List<Utilisateur> Utilisateurs
         {
             get { return _utilisateurs; }
