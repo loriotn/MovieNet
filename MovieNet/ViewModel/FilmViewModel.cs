ce03b9c71048a06e0fc3be731608b92589bfdc0a
unknown <loriot_n@etna-alternance.net>
2017-08-27 22:51:50 +0100

    add full navigation behavior

---
+++ MovieNet/ViewModel/FilmViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MovieNetApiWcf;
+
+namespace MovieNet.ViewModel
+{
+    public class FilmViewModel : MainViewModel
+    {
+        public FilmViewModel(Service1 service) : base(service)
+        {
+
+        }
+    }
+}
