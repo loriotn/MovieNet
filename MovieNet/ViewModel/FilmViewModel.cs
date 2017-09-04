b424c70c03c9dbf7aef1048cf1f52df79d38eaf8
unknown <loriot_n@etna-alternance.net>
2017-09-04 23:36:05 +0100

    add new view movie

--- MovieNet/ViewModel/FilmViewModel.cs
+++ MovieNet/ViewModel/FilmViewModel.cs
@@ -11,6 +11,15 @@ namespace MovieNet.ViewModel
 {
     public class FilmViewModel : MainViewModel
     {
+        public double HeightGridMovie { get; set; }
+        public double HeightTitle { get; set; }
+        public double HeightComment { get; set; }
+        public double HeightNewComment { get; set; }
+        public double WidthGridMovie { get; set; }
+        public double WidthGridMovieComment { get; set; }
+        public double WidthButtons { get; set; }
+        public double WidthAreaComment { get; set; }
+
         private double heightMovie;
 
         public double HeightMovie
@@ -62,8 +71,16 @@ namespace MovieNet.ViewModel
 
         public FilmViewModel()
         {
-            HeightMovie = m.Height * 0.78;
+            HeightMovie = m.Height * 0.85;
             WidthMovie = m.Width - 20;
+            HeightGridMovie = HeightMovie - HeightMovie * 0.08;
+            HeightTitle = HeightMovie * 0.08;
+            HeightNewComment = HeightMovie * 1 / 3;
+            HeightComment = HeightMovie * 2 / 3;
+            WidthGridMovie = WidthMovie * 3 / 9;
+            WidthGridMovieComment = WidthMovie * 4 / 9;
+            WidthButtons = WidthMovie * 1 / 9;
+            WidthAreaComment = WidthMovie - WidthGridMovie;
             Film = new Film();
             Films = Facade.filmService.GetAll();
         }
